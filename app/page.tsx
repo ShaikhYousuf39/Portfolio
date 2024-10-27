14d5f2f11ca092b66beeb7bc86d4a75a329a9efe
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-10-27 14:50:24 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         className="flex flex-col-reverse md:flex-row items-center justify-around text-center md:text-left py-20 px-6"
       >
         <section className="flex flex-col items-center justify-center text-center py-20">
-          <h1 className="text-4xl md:text-6xl font-bold leading-tight tracking-tight">Hello, I’m Yousuf!</h1>
+          <h1 className="text-5xl md:text-6xl font-bold leading-tight tracking-tight">Hello, I’m Yousuf!</h1>
           <p className="text-xl text-gray-700 max-w-2xl">
             I’m a passionate developer specializing in web development and
             graphic design. Let’s build something amazing together.
@@ -64,7 +64,7 @@ export default function Home() {
           <div className="mt-8">
             <a
               href="#projects"
-              className="px-6 py-3 bg-gray-800 text-white rounded-full shadow hover:bg-gray-900 transition"
+              className="px-3 py-2 md:px-6 md:py-3 bg-gray-800 text-white rounded-full shadow hover:bg-gray-900 transition"
             >
               View My Work
             </a>
